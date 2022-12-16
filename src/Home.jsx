980b601b2a4a9b5be74ad6b5088778e605cdfd12
yarazarin <uraeel@gmail.com>
2022-12-16 12:21:54 -0500

    nahamber nav added

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,10 +5,13 @@ import UrHere from "./Components/UrHere/UrHere";
 import Gap from "./Components/Gap/Gap";
 import Updown from "./Components/UpDown/UpDown";
 import Bubbles from "./Components/Bubbles/Bubbles";
+import HamberMenu from "./Components/HamberMenu/HamberMenu";
+import "./Home.css"
 
 const Home = () => {
   return (
-    <div>
+    <div className="main-home">
+      <HamberMenu/>
       <UrHere pageName="Home_Demo_Website" />
       <NavBar />
       <Updown />
